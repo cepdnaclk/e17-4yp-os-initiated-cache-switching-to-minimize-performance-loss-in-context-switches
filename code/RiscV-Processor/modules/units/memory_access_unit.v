8b60b1590d0ce0e01f07050913f37c17587301cf
AchinthaHarshamal <achinthafharshamal@gmail.com>
2023-08-02 15:56:58 +0530

    tested mem

--- code/RiscV-Processor/modules/units/memory_access_unit.v
+++ code/RiscV-Processor/modules/units/memory_access_unit.v
@@ -18,7 +18,21 @@ module memory_access_unit (
     Data_load_controller dlc(func3,from_data_cache_out,load_data);
 
     // dcache mydcache(clock,reset,mem_read_signal,mem_write_signal,mux4_out_result,store_data,from_data_cache_out,data_memory_busywait);
-    Cache_controller myCache_controller(clock,reset,mem_read_signal,mem_write_signal,mux4_out_result,store_data,from_data_cache_out,data_memory_busywait,func3_cache_select_reg_value,write_cache_select_reg);
+    Cache_controller myCache_controller(
+        // inputs
+        clock,
+        reset,
+        mem_read_signal,
+        mem_write_signal,
+        mux4_out_result,
+        store_data,
+        // outputs
+        from_data_cache_out,
+        data_memory_busywait,
+        // inputs
+        func3_cache_select_reg_value,
+        write_cache_select_reg
+        );
     mux2x1 mux5(load_data,mux4_out_result,mux5signal,mux5_out_write_data);
 
 endmodule
