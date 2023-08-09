04240c6d930af3c9a48c390611b73418dd6c86ab
Sandun666 <e17168@eng.pdn.ac.lk>
2023-08-10 03:03:38 +0530

    hazard unit

---
+++ code/RiscV-Processor/modules/hazard-detection-correction/Ex_forward_unit.v
@@ -0,0 +1,39 @@
+module Ex_forward_unit(
+    // inputs
+    wb_address_MEM,
+    wb_write_en_MEM,
+    wb_address_WB,
+    wb_write_en_WB,
+    address1_EX,
+    address2_EX,
+    // outputs
+    data1_forward_select,
+    data2_forward_select
+);
+
+input wb_write_en_MEM, wb_write_en_WB;
+input [4:0] wb_address_MEM, wb_address_WB, address1_EX, address2_EX;
+
+output reg [1:0] data1_forward_select, data2_forward_select;
+     
+    always @ (*)
+    begin
+        
+        // Forwarding for Operand 1
+        if (wb_write_en_MEM && wb_address_MEM === address1_EX)
+            data1_forward_select = 2'b01;    // Activate forwarding from MEM stage
+        else if (wb_write_en_WB && wb_address_WB === address1_EX)
+            data1_forward_select = 2'b10;    // Activate forwarding from WB stage
+        else
+            data1_forward_select = 2'b00;    // No forwarding
+
+        // Forwarding for Operand 2
+        if (wb_write_en_MEM && wb_address_MEM === address2_EX)
+            data2_forward_select = 2'b01;    // Activate forwarding from MEM stage
+        else if (wb_write_en_WB && wb_address_WB === address2_EX)
+            data2_forward_select = 2'b10;    // Activate forwarding from WB stage
+        else
+            data2_forward_select = 2'b00;    // No forwarding
+    end
+
+endmodule
---
+++ code/RiscV-Processor/modules/hazard-detection-correction/Flush_unit.v
@@ -0,0 +1,23 @@
+module Flush_unit(
+    // inputs
+    hazard_detect,
+    bj_mux_select,
+    // outputs
+    reset_ID_reg,
+    reset_IF_reg,
+    hold_IF_reg
+);
+
+input hazard_detect, bj_mux_select;
+output reg reset_ID_reg, reset_IF_reg, hold_IF_reg;
+
+// In case of a branch/jump, IF/ID PR must be reset
+// In case of a load-use hazard, IF/ID PR must hold its value
+assign reset_IF_reg = bj_mux_select;
+assign hold_IF_reg = !bj_mux_select && hazard_detect;
+
+// In case either a branch/jump or load-use hazard occurs,
+// ID/EX PR must be reset
+assign reset_Id_reg = bj_mux_select || hazard_detect ;
+
+endmodule
