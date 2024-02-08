b88760cbdcde4e31290b19e753dbd54421b2307e
Arshad-MRM <e17015@eng.pdn.ac.lk>
2024-02-08 23:36:55 +0530

    testing

---
+++ test/Compiler Program03 (triangle)/trino.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n = 6;
+
+    int sum = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+    
+    // printf("The %dth triangle number is: %d\n", n, sum);
+
+    return 0;
+}
